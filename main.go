f7b77d5de5c55599f3e1147f4420889a9676668f
seike460 <seike460@gmail.com>
2020-06-19 16:14:00 +0900

    :sparkles: 機能追加

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"C"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -14,24 +15,43 @@ var writer = os.Stdout
 var cpus = runtime.NumCPU()
 
 func main() {
+	// CPUの数を取得
 	runtime.GOMAXPROCS(cpus)
-	dirwalk(string("."))
+	// 自分のいるディレクトリを取得
+	// @TODO 引数検索するディレクトリを指定する
+	dir, err := os.Getwd()
+	if err != nil {
+		//
+		log.Fatal(err)
+	}
+	// ディレクトリ内を探索
+	dirwalk(dir)
 }
 
-func dirwalk(dir string) {
+// export dirwalk
+func dirwalk(dir string) string {
+	// 指定されたディレクトリもある fileInfoの一覧を取得する
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		log.Fatal(err)
 	}
-	semaohorer := make(chan int, cpus)
+	// CPU
+	ch := make(chan int, cpus)
 	wg := sync.WaitGroup{}
+	// ファイルの一覧をループ
 	for _, file := range files {
+		// ディレクトリだったら
 		if file.IsDir() {
 			wg.Add(1)
-			go deepDirwalk(dir, file, &wg, semaohorer)
+			// 更に中を
+			go deepDirwalk(dir, file, &wg, ch)
+		} else {
+			path := filepath.Join(dir, file.Name())
+			fmt.Println(path)
 		}
 	}
 	wg.Wait()
+	return ""
 }
 
 func deepDirwalk(dir string, file os.FileInfo, wg *sync.WaitGroup, ch chan int) {
