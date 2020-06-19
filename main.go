4cbf414aedb38b31f9b026f38cd1730c5997d0b8
seike460 <seike460@gmail.com>
2020-06-19 16:25:15 +0900

    :sparkles: 機能追加

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"C"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -21,7 +20,7 @@ func main() {
 	// @TODO 引数検索するディレクトリを指定する
 	dir, err := os.Getwd()
 	if err != nil {
-		//
+		// log出力して強制終了
 		log.Fatal(err)
 	}
 	// ディレクトリ内を探索
@@ -29,36 +28,40 @@ func main() {
 }
 
 // export dirwalk
-func dirwalk(dir string) string {
+func dirwalk(dir string) {
 	// 指定されたディレクトリもある fileInfoの一覧を取得する
 	files, err := ioutil.ReadDir(dir)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// CPU
+	// CPU 分のchannelを作る
 	ch := make(chan int, cpus)
 	wg := sync.WaitGroup{}
 	// ファイルの一覧をループ
 	for _, file := range files {
-		// ディレクトリだったら
 		if file.IsDir() {
+			// ディレクトリだったら
+			// WaitGroupを追加して、goroutine発行し、自分自身は処理を続行
 			wg.Add(1)
-			// 更に中を
 			go deepDirwalk(dir, file, &wg, ch)
 		} else {
+			// ファイルだったら、標準出力して終わり
 			path := filepath.Join(dir, file.Name())
-			fmt.Println(path)
+			fmt.Fprintln(writer, path)
 		}
 	}
+	// WaitGroupが0になるまで待つ
 	wg.Wait()
-	return ""
 }
 
 func deepDirwalk(dir string, file os.FileInfo, wg *sync.WaitGroup, ch chan int) {
+	// deepDirwalkが終わったら、WaitGroupをへらす
 	defer wg.Done()
+	// channelに値を入れる
 	ch <- 1
 	path := filepath.Join(dir, file.Name())
 	fmt.Fprintln(writer, path)
 	dirwalk(path)
+	// channelの値を捨ててブロック解除
 	<-ch
 }
